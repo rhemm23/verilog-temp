d3fe83c9c661276ad515dd00f9a25a189bedbe12
rhemm23 <ryanhemmila@gmail.com>
2021-11-27 23:53:24 -0600

    prog

--- sw/main.c
+++ sw/main.c
@@ -6,6 +6,20 @@
 
 #include "afu_json_info.h"
 
+static void close_fpga_handle(fpga_handle handle) {
+  if (fpgaClose(handle) != FPGA_OK) {
+    fprintf(stderr, "Failed to close fpga handle\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+static void destroy_token_object(fpga_token *token) {
+  if (fpgaDestroyToken(token) != FPGA_OK) {
+    fprintf(stderr, "Failed to destroy token object\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
 static void destroy_properties_object(fpga_properties *filter) {
   if (fpgaDestroyProperties(filter) != FPGA_OK) {
     fprintf(stderr, "Failed to destroy properties object\n");
@@ -16,9 +30,11 @@ static void destroy_properties_object(fpga_properties *filter) {
 int main() {
 
   fpga_properties filter = NULL;
+  fpga_handle handle;
   fpga_token token;
   fpga_guid guid;
 
+  uint64_t *mmio_space = NULL;
   uint32_t num_matches = 0;
   
   if (uuid_parse(AFU_ACCEL_UUID, guid) < 0) {
@@ -49,7 +65,27 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  printf("Number of matches: %d\n", num_matches);
+  if (fpgaOpen(token, &handle, 0) != FPGA_OK) {
+    destroy_token_object(&token);
+    destroy_properties_object(&filter);
+    fprintf(stderr, "Failed to open fpga\n");
+    return EXIT_FAILURE;
+  }
+
+  if (fgpaMapMMIO(handle, 0, &mmio_space) != FPGA_OK) {
+    close_fpga_handle(handle);
+    destroy_token_object(&token);
+    destroy_properties_object(&filter);
+    fprintf(stderr, "Failed to map mmio space\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("Low guid bytes: %llx\n", mmio_space[1]);
+  printf("High guid bytes: %llx\n", mmio_space[2]);
+
+  close_fpga_handle(handle);
+  destroy_token_object(&token);
+  destroy_properties_object(&filter);
 
   return 0;
 }
